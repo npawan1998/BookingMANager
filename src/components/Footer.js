1ed67b114cd7daa680b920696c56cfd7e59676ca
Vamsi manohar <vamsimonohar@gmail.com>
2021-10-29 22:19:37 +0530

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
+        </div>
+    )
+}
+
+export default Footer
